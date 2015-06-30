aaeb2b51f2771446b9a9e74e4401e894931f5140
Vincenzo Marrazzo <pariamentz@gmail.com>
2015-06-30 13:39:19 +0200

    small makeup reduced number of test refactoring generateActions generator

--- src/test/scala/ch3/Test_ch3ex2.scala
+++ src/test/scala/ch3/Test_ch3ex2.scala
@@ -9,7 +9,7 @@ import org.scalacheck.Arbitrary._
 import org.scalacheck.Gen
 import org.scalatest.concurrent.Timeouts
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
-import org.scalatest.{MustMatchers, FlatSpec}
+import org.scalatest.{ MustMatchers, FlatSpec }
 
 import org.learningconcurrency.log
 
@@ -17,33 +17,74 @@ import org.learningconcurrency.log
 
 import ch2.thread
 
-import scala.collection.mutable.{Set, Stack}
+import scala.collection.mutable.{ Set, Stack }
 
 class Test_ch3ex2 extends FlatSpec
-with MustMatchers
-with Timeouts
-with GeneratorDrivenPropertyChecks {
+  with MustMatchers
+  with Timeouts
+  with GeneratorDrivenPropertyChecks {
 
   behavior of "ch3ex2"
 
   /**
-   *
+   * Generator section
    */
-  implicit override val generatorDrivenConfig = PropertyCheckConfig(minSuccessful = 4, workers = 4)
 
   /**
-   *
+   * It is the generic action performed by thread
    */
-  private val random = scala.util.Random
-
   sealed trait ActionType
 
+  /**
+   * Push test action
+   */
   case object PushAction extends ActionType
 
+  /**
+   * Pop test action
+   */
   case object PopAction extends ActionType
 
   /**
+   * Random test matrix can in theory empty under test data structure so before real test fill with some data 
+   */
+  val setupStep = 100
+
+  /**
+   * Number of thread 
+   */
+  val numbThread = 10
+
+  /**
+   * Number of step for each thread 
+   */
+  val threadStep = 10
+
+  /**
+   * Thread action matrix generator
    *
+   * @return a test matrix with one row of actions for each thread
+   */
+  val generateActions = Gen.listOfN(numbThread,
+    Gen.listOfN(threadStep,
+      Gen.oneOf(PushAction, PopAction)) suchThat (_.length.equals(threadStep))) suchThat (_.length.equals(numbThread))
+
+  /**
+   * Customize the number of test into ScalaCheck environment
+   */
+  implicit override val generatorDrivenConfig = PropertyCheckConfig(minSuccessful = 1, workers = 1)
+
+  /**
+   * Generator section - END
+   */
+
+  /**
+   * Random number generator only to fill data structure
+   */
+  private val random = scala.util.Random
+
+  /**
+   * Used to reorder test data for verification steps
    */
   val testStepGenerator: AtomicInteger = new AtomicInteger(0)
 
@@ -89,148 +130,131 @@ with GeneratorDrivenPropertyChecks {
       }
     }
 
-  it should "works with multi executor and scala check" in {
-
-    /**
-     * Generator section
-     */
-
-    val setupStep = 100
-    val actionStepMax = 10
-    val numbThread = 10
-
-    /**
-     * Thread action matrix generator
-     *
-     * @return
-     */
-    def generateActions = Gen.sized { size => Gen.listOfN(numbThread, Gen.listOfN(actionStepMax, Gen.oneOf(PushAction, PopAction)) suchThat (_.length == actionStepMax)) } suchThat (_.length == numbThread)
-
-    /**
-     * Generator section - END
-     */
+  /**
+   * The main test case
+   */
+  it should "works with multi executor and Scala check" in {
 
     forAll((generateActions, "Threads action matrix")) {
 
-      (actionArrays: List[List[ActionType]]) => {
+      (actionArrays: List[List[ActionType]]) =>
+        {
 
-        /**
-         * Under test object
-         */
-        val underTest = new TreiberStack[Int]
+          /**
+           * Under test object
+           */
+          val underTest = new TreiberStack[Int]
 
-        /**
-         * Verification data structure
-         */
-        val testCheck = Set.empty[TestActionLog]
+          /**
+           * Verification data structure
+           */
+          val testCheck = Set.empty[TestActionLog]
 
-        /**
-         * Fill the under test stack with some data
-         */
-        for (i <- 1 to setupStep) {
-          val next = random.nextInt(100)
-          underTest.push(next)
+          /**
+           * Fill the under test stack with some data
+           */
+          for (i <- 1 to setupStep) {
+            val next = random.nextInt(100)
+            underTest.push(next)
 
-          log(s"pre-setup $i push operation of value $next")
-          val timestamp = System.currentTimeMillis
+            log(s"pre-setup $i push operation of value $next")
+            val timestamp = System.currentTimeMillis
 
-          testCheck += TestActionLog(testStepGenerator.incrementAndGet, PushAction, next)
-        }
-
-        /**
-         * Timed area
-         */
-
-        failAfter(2 seconds) {
-
-          implicit val printLog: Boolean = false
-
-          val group = for (actions <- (actionArrays zipWithIndex)) yield thread(s"Sample${actions._2}", runnable(actions._1, underTest, testCheck))
-
-          for (t <- group) t.start
-
-          var exit = false
-
-          while (!exit) {
-            exit = group.map(_.isAlive).filter(_ == true).isEmpty
+            testCheck += TestActionLog(testStepGenerator.incrementAndGet, PushAction, next)
           }
-        }
 
-        /**
-         * Timed area - END
-         */
+          /**
+           * Timed area
+           */
 
-        /**
-         * Verification
-         */
+          failAfter(2 seconds) {
 
-        /**
-         * Build a stack based on test log obtained
-         */
-        val testStack: Stack[Int] = {
+            implicit val printLog: Boolean = false
 
-          val resp = new Stack[Int]
-          val actionLog = testCheck.toList.sortBy(_.testStep)
+            val group = for (actions <- (actionArrays zipWithIndex)) yield thread(s"Sample${actions._2}", runnable(actions._1, underTest, testCheck))
 
-          for (act <- actionLog)
-            act match {
-              case TestActionLog(_, PushAction, value) => resp.push(value)
-              case TestActionLog(_, PopAction, value) => resp.pop
+            for (t <- group) t.start
+
+            var exit = false
+
+            while (!exit) {
+              exit = group.map(_.isAlive).filter((b: Boolean) => b).isEmpty
+            }
+          }
+
+          /**
+           * Timed area - END
+           */
+
+          /**
+           * Verification
+           */
+
+          /**
+           * Build a stack based on test log obtained
+           */
+          val testStack: Stack[Int] = {
+
+            val resp = new Stack[Int]
+            val actionLog = testCheck.toSeq.sortBy(_.testStep)
+
+            for (act <- actionLog)
+              act match {
+                case TestActionLog(_, PushAction, value) => resp.push(value)
+                case TestActionLog(_, PopAction, value) => resp.pop
+              }
+
+            resp
+          }
+
+          /**
+           * Verify built stack if has expected size
+           */
+          testStack must have size {
+
+            var totalAction = 0
+            var pushAction = 0
+
+            for (act <- actionArrays) {
+              totalAction += act.length
+              pushAction += act.filter(_ eq PushAction).length
             }
 
-          resp
-        }
-
-        /**
-         * verify builded stack if has expected size
-         */
-        testStack must have size {
-
-          var totalAction = 0
-          var pushAction = 0
-
-          for (act <- actionArrays) {
-            totalAction += act.length
-            pushAction += act.filter(_ eq PushAction).length
+            setupStep + 2 * pushAction - totalAction
           }
 
-          setupStep + 2 * pushAction - totalAction
-        }
+          /**
+           * verify that each element into built stack is inside underTest
+           */
+          for (i <- testStack) {
 
-        /**
-         * verify that each element into built stack is inside underTest
-         */
-        for (i <- testStack) {
+            val popped = underTest.pop
 
-          val popped = underTest.pop
-
-          withClue(s"underTest -> $underTest\n testStack -> $testStack : ")
-          {
-            popped must be(i)
+            withClue(s"underTest -> $underTest\n testStack -> $testStack : ") {
+              popped must be(i)
+            }
+            log(s"$popped is correct")
           }
-          log(s"$popped is correct")
+
+          /**
+           * another pop on underTest and we obtain exception
+           */
+          try {
+            underTest.pop
+
+            fail("under test stack must be empty now!")
+          } catch {
+            case _: Exception => assert(true)
+          }
+
+          /**
+           * Verification - END
+           */
+
         }
-
-        /**
-         * another pop on underTest and we obtain exception
-         */
-        try {
-          underTest.pop
-
-          fail("under test stack must be empty now!")
-        }
-        catch {
-          case _ : Exception => assert(true)
-        }
-
-        /**
-         * Verification - END
-         */
-
-      }
 
     }
 
-  }
+  } // end main test case
 
 }
