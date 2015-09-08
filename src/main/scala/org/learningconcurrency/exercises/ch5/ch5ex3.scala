2a5bd7a7b02359d2aa1676c0d2d9351a367af061
Vincenzo Marrazzo <pariamentz@gmail.com>
2015-09-08 14:25:16 +0200

    completed first part on set generation refactoring code to better investigate plotting part

--- src/main/scala/org/learningconcurrency/exercises/ch5/ch5ex3.scala
+++ src/main/scala/org/learningconcurrency/exercises/ch5/ch5ex3.scala
@@ -4,6 +4,8 @@ import breeze.math.Complex
 import scala.swing.{ BorderPanel, Component, MainFrame }
 import java.awt.{ Color, Dimension, Graphics2D }
 
+import scala.collection._
+
 import org.learningconcurrency.ch5._
 
 object ch5ex3 {
@@ -13,14 +15,14 @@ object ch5ex3 {
    * - a square area described for highest (up/right) and lowest (down/left) points
    * - final plot size in pixels
    */
-  val high = Complex(1.0, 1.0)
-  val low = Complex(-1.0, -1.0)
+  val high = Complex(2.0, 2.0)
+  val low = Complex(-2.0, -2.0)
 
   /**
    * This values describe convergenceMap area resolution in pixel
    */
-  val width: Int = 640
-  val height: Int = 480
+  val width: Int = 800
+  val height: Int = 600
 
   /**
    * If a Mandelbrot set reach this number without diverge is convergenceMapted as belong it
@@ -57,14 +59,83 @@ object ch5ex3 {
     // evaluate convergence
     stream.find(diverge) match {
       case Some(index) => Some(stream.indexOf(index)) // diverge so will be convergenceMapted
-      case None => None // never diverge -> black
+      case None        => None // never diverge -> black
     }
   }
 
   /**
+   * It used to encapsulate plotting to measure its time
+   */
+  private def plotFrame(convergenceMap: GenMap[Complex, Option[Int]], complex2Pixel: Map[Complex, (Int, Int)], pTitle: String = "ch5x3", dispose: Boolean = true): Unit = {
+
+    val mFrame = new MainFrame {
+
+      title = pTitle
+      preferredSize = new Dimension(width, height)
+      visible = true
+      centerOnScreen
+
+      def calculateColor(convIndex: Option[Int]): Color = {
+        convIndex match {
+          case Some(iter) => {
+
+            // iter is contained into (0,maxConvergenceThreshold)
+            val c = 3 * math.log(iter + 1.0) / math.log(maxConvergenceThreshold)
+
+            val (r, g, b) =
+              if (c < 1)
+                ((255 * c).toInt, 0, 0)
+              else if (c < 2)
+                (255, (255 * (c - 1)).toInt, 0)
+              else
+                (255, 255, (255 * (c - 2)).toInt)
+
+            //import org.learningconcurrency._
+            //log(s"Index -> ${iter} to ($r,$g,$b)")                      
+
+            new Color(r, g, b)
+          }
+          case None => Color.BLACK
+        }
+      }
+
+      contents = new BorderPanel {
+        add(new Component {
+          override def paintComponent(g: Graphics2D) {
+
+            convergenceMap foreach {
+              case (complex, indexConv) => {
+                g.setColor(calculateColor(indexConv))
+                complex2Pixel.get(complex) match {
+                  case Some((x, y)) => g.drawLine(x, y, x, y)
+                  case None         => ??? // Impossible condition
+                }
+              }
+            }
+
+          }
+        }, BorderPanel.Position.Center)
+      }
+    } // end main frame
+
+    Thread.sleep(2000)
+
+    if (dispose)
+      mFrame.dispose
+  }
+
+  /**
+   *
+   *
    * Main
+   *
+   *
    */
   def main(args: Array[String]): Unit = {
+
+    println(s"Plot resolution ($width,$height)")
+    println(s"Bottom left coordinate -> ${low}")
+    println(s"Up right coordinate -> ${high}")
 
     // the last "take" is to ensure that points are equals to expected pixels
     val xrange = (low.real to high.real by (high.real - low.real) / width).take(width)
@@ -75,21 +146,21 @@ object ch5ex3 {
      * - Complex coordinate for mathematical calculation
      * - Pixel coordinate for plotting
      */
-    val complex2PixelSeq = for {
+    val complex2PixelPairs = for {
       (x, xpixel) <- xrange.zipWithIndex
       (y, ypixel) <- yrange.reverse.zipWithIndex // in that manner left most corner is starting point
     } yield (Complex(x, y), (xpixel, ypixel))
 
     // Map from Complex plane to Pixel plane
-    val complex2Pixel: Map[Complex, (Int, Int)] = complex2PixelSeq.toMap
+    val complex2Pixel: Map[Complex, (Int, Int)] = complex2PixelPairs.toMap
 
     // the complex point for mathematical calculation
     val complexPlanCoordinate = complex2Pixel.keys.toVector
 
-    import scala.collection.parallel.immutable.ParMap
+    // GenMap will cover both sequential and parallel data test
 
-    var convergenceMap: Map[Complex, Option[Int]] = null
-    var convergenceMapPar: ParMap[Complex, Option[Int]] = null
+    var convergenceMap: GenMap[Complex, Option[Int]] = null
+    var convergenceMapPar: GenMap[Complex, Option[Int]] = null
 
     val creationConvergenceMapSeq = timed {
       // This map contains the coordinate according convergence (None) and divergence (Some(s))
@@ -109,21 +180,19 @@ object ch5ex3 {
       }.toMap
     }
 
+    val divergeElemSeq = convergenceMap.filter { case ((k, v)) => v != None }.size
+    val convergElemSeq = convergenceMap.filter { case ((k, v)) => v == None }.size
+
+    val divergeElemPar = convergenceMapPar.filter { case ((k, v)) => v != None }.size
+    val convergElemPar = convergenceMapPar.filter { case ((k, v)) => v == None }.size
+
+    assert(divergeElemSeq == divergeElemPar, s"Divergence points does not match (Seq=$divergeElemSeq,Par=$divergeElemPar)")
+    assert(convergElemSeq == convergElemPar, s"Convergence points does not match (Seq=$convergElemSeq,Par=$convergElemPar)")
+
+    println("Convergence sets (Sequentil and Parallel) are equals.")
     println(s"Convergence obtained sequentially $creationConvergenceMapSeq ms")
     println(s"Convergence obtained parallel $creationConvergenceMapPar ms")
 
-    //val toBeColored = convergenceMap.filter { case ((k, v)) => v != None }
-    val toBeBlack = convergenceMap.filter { case ((k, v)) => v == None }.keys // no added value with map
-
-    /*
-    println(s"Plot resoution ($width,$height)")
-    println(s"xrange points -> ${xrange.size}")
-    println(s"yrange points -> ${yrange.size}")
-    println(s"Total number of expected points -> ${width * height}")
-    println(s"Obtained points -> ${convergenceMap.size}")
-    println(s"Point that diverge and will be colored -> ${toBeColored.size}")
-    */
-  
     /*
     import java.io._
     val pw = new PrintWriter(new File("toBeColored.txt"))
@@ -131,44 +200,10 @@ object ch5ex3 {
     pw.close
     */
 
-    val mFrame = new MainFrame {
-      title = "ch5x3"
-      preferredSize = new Dimension(width, height)
-      visible = true
-      centerOnScreen
-      contents = new BorderPanel {
-        add(new Component {
-          override def paintComponent(g: Graphics2D) {
+    val plottingSeq = timed { plotFrame(convergenceMap, complex2Pixel, "ch5x3 Sequential") }
+    val plottingPar = timed { plotFrame(convergenceMapPar, complex2Pixel, "ch5x3 Parallel") }
 
-            def calculateColor(convIndex: Option[Int]): Color = {
-              convIndex match {
-                case Some(iter) => {
-                  val c = 3*math.log(iter)/math.log(maxConvergenceThreshold-1.0)
-                  if(c<1) new Color((255*c).toInt, 0, 0)
-                  else if(c<2) new Color(255, (255*(c-1)).toInt, 0)
-                  else new Color(255, 255, (255*(c-2)).toInt)
-                }
-                case None => Color.BLACK
-              }
-            }
-
-            convergenceMap foreach {
-              case ( complex, indexConv) => {
-                g.setColor(calculateColor(indexConv))
-                complex2Pixel.get(complex) match {
-                  case Some((x, y)) => g.drawLine(x, y, x, y)
-                  case None => ??? // Impossible condition
-                }
-              }
-            }
-
-          }
-        }, BorderPanel.Position.Center)
-      }
-    } // end main frame
-
-    Thread.sleep(2000)
-
-    mFrame.dispose
+    println(s"Plotting sequentially $plottingSeq ms")
+    println(s"Plotting parallel $plottingPar ms")
   }
 }
